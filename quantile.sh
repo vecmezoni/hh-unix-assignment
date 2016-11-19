bbfb2aa06258e674e86575c2f7ab89c03b6cb5c3
jet_vorobev <ia.vorobev@jet.msk.su>
2016-11-19 20:48:21 +0300

    посчитал квантиль с и без awk

--- quantile.sh
+++ quantile.sh
@@ -1,5 +1,38 @@
 #!/bin/sh
 
-# Здесь ожидается решение c использованием awk
+quantile_95(){
+  cnt=0
+  while read -r data
+  do
+	  if [ $cnt -eq 0 ]
+	  then
+	   set "$data"
+	  else
+	   set -- "$@" "$data"
+	  fi 
+	  cnt=$((cnt + 1))
+  done
+ 
+  DIV=$((cnt * 95 / 100))
+  MOD=$((cnt * 95 % 100))
+  if [ $MOD != 0 ]
+  then
+	DIV=$((DIV + 1))
+  fi  
+ 
+ cnt=0 
+ for i do
+	if [ $cnt -eq $((DIV - 1)) ]
+	then
+	  echo "$i"
+	  break
+	fi
+	cnt=$((cnt + 1))
+  done
+}
+
+# Здесь ожидается решение без использования awk
+
+sed -n 's/.*\"\[\(.*\)\]\".*/\1/p' | sort -n | quantile_95
 
 exit 0
