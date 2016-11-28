7f7ec104ffa28753307651b26cd81b4ee30bc913
b.khasanov <afti@yandex.ru>
2016-11-29 02:25:38 +0300

    Расчет awk перцентили Добавил решение на bash, совместимое с POSIX sh.

--- quantile.sh
+++ quantile.sh
@@ -1,5 +1,41 @@
 #!/bin/sh
 
-# Здесь ожидается решение c использованием awk
+quantile() {
+    i=0
+    alpha=0.95
+    while read -r data; do
+        set -- "$@" "$data"
+        i=$((i + 1))
+    done
+    K=$(((i - 1) * 95 / 100))
+    K_next=$((K + 1))
+    position=$(echo "$i * $alpha" | bc -l)
+
+    count=0
+    for i do
+        if [ "$count" -eq "$K" ]
+        then
+            K_value="$i"
+        elif [ "$count" -eq "$K_next" ]
+        then
+            K_next_value="$i"
+            break
+        fi
+        count=$((count + 1))
+    done
+
+
+    if [ "$(echo "$K_next < $position" | bc -l )" ]
+    then
+        echo "$K_next_value"
+    elif [ "$(echo "$K_next == $position" | bc -l )" ]
+    then
+        echo "($K_value + $K_next_value) / 2" | bc -l
+    else
+        echo "$K_value"
+    fi
+}
+
+sed -n 's/.*\"\[\([0-9.]*\)\]\".*/\1/p' | sort -n | quantile
 
 exit 0
--- quantile_awk.sh
+++ quantile_awk.sh
@@ -1,5 +1,18 @@
 #!/bin/sh
 
-# Здесь ожидается решение без использования awk
+awk 'match($0, /"\[[.0-9]+\]"/) { print substr($0, RSTART + 2, RLENGTH - 4)|"sort -n" };' |
+awk '
+{response_time[NR - 1] = $0}
+END{
+alpha = 0.95;
+K = int((NR - 1) * alpha);
+K_next = K + 1;
+position = alpha * NR;
+if (K_next < position) result = response_time[K_next]
+else if (K_next == position) result = (response_time[K] + response_time[K_next]) / 2;
+else result = response_time[K];
+print result;
+}
+'
 
 exit 0
