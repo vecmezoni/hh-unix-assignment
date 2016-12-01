708ceb5bc9a50203181d910f6461d1263464862e
Alexey-Trifonov <alekseitr@gmail.com>
2016-12-02 01:29:27 +0300

    Update quantile.sh

--- quantile.sh
+++ quantile.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
-# Здесь ожидается решение c использованием awk
+# Здесь ожидается решение без использованием awk
 
-exit 0
+LINES=$(sed 's/.*\(\[[.0-9]\+\]\).*/\1/' | sed 's/\[\(.*\)\]/\1/' | sort)
+IFS=$' '
+NUM_LINES=$(echo "$LINES" | wc -l)
+TARGET=$(echo "($NUM_LINES*0.95 + 0.5)/1" | bc)
+echo "$LINES" | sed "${TARGET}q;d"
+unset IFS
