6743898b8580dae6fa4cabb1854eb3262e7619b0
Trympyrym <rio09876@gmail.com>
2016-12-02 10:39:44 +0200

    added solution without awk usage

--- quantile.sh
+++ quantile.sh
@@ -1,5 +1,11 @@
-#!/bin/sh
+#!/bin/bash
 
-# Здесь ожидается решение без использованиея awk
+# Здесь ожидается решение без использования awk
 
+declare -a list=($(grep -o "\"\[[.0-9]*\]\"" $1 | cut -c 3- | rev | cut -c 3- | rev | sort -n))
+
+length=${#list[@]}
+index=$((length*19/20))
+quantile=${list[$((index-1))]}
+echo $quantile
 exit 0
