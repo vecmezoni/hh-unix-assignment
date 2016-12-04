2d43c94299a8128e4c9e4340e6d8f18f1009abd2
alexandermedvedev <alex.medvedev@hotmail.com>
2016-12-04 12:53:44 +0000

    Homework commit

--- quantile.sh
+++ quantile.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
 # Здесь ожидается решение c использованием awk
-
+declare -a time=($(grep -o "\"\[[.0-9]*\]\"" "$1" | grep -o "[.0-9]*" | sort -n))
+length=${#time[@]}
+index=$(((length*95+50)/100))
+result=${time[$((index-1))]}
+echo "$result"
 exit 0
