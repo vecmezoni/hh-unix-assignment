dad415b0ede7415bfcab40c485a351f525b5e48c
Sergey Savin <savin.sergey91@gmail.com>
2016-12-06 16:57:56 +0300

    Решение дз

--- quantile.sh
+++ quantile.sh
@@ -1,5 +1,9 @@
-#!/bin/sh
+﻿#!/bin/sh
 
-# Здесь ожидается решение c использованием awk
+# Здесь ожидается решение без использования awk
 
-exit 0
+CNT=`wc -l < /path/to/log.txt`
+ 
+DONE=$((CNT * 95 / 100 + 1))
+ 
+cat /path/to/log.txt |  cut -f 3 -d '[' | cut -f 1 -d ']' | sort | sed "${DONE}q;d"
